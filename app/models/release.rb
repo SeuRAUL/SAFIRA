2f520678b21b47a4a425e1fecddb44c16a9ffc4b
andrezap <andrezapbl1@gmail.com>
2013-10-20 22:01:05 -0300

    tdd of release

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,9 +1,15 @@
 class Release < ActiveRecord::Base
   attr_accessible :date_release, :description, :doc_number, :doc_type, :form_payment, :origin_destination, :type_release, :value
   
+  possible_form_payment = ['Cartão', 'Dinheiro', 'Boleto', 'Cheque' ]
+  possible_doc_type = ['Exemplo 1, Exemplo 2']
+
   validates :value, presence: true, numericality: true #, message: "Precisa constar o valor da nota."
   validates :description, presence: true #, message: "Precisa constar a descrição da nota."
   validates :doc_number, presence: true, uniqueness: true #, message: "Precisa constar o numero do documento."
-
+  validates :form_payment, inclusion_of: possible_form_payment
+  validates :doc_type, inclusion_of: possible_doc_type
   belongs_to :cashier
+
+
 end
