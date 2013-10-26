80d836da9a5a9a6c7a19b33e59b4e524c9ff57f4
andrezap <andrezapbl1@gmail.com>
2013-10-26 10:30:49 -0300

    fix validation

--- app/models/release.rb
+++ app/models/release.rb
@@ -2,14 +2,14 @@
 class Release < ActiveRecord::Base
   attr_accessible :date_release, :description, :doc_number, :doc_type, :form_payment, :origin_destination, :type_release, :value
   
-  possible_form_payment = ["Cartao", "Dinheiro", "Boleto", "Cheque"]
-  #possible_doc_type = ['Exemplo 1, Exemplo 2']
+  POSSIBLE_FORM_PAYMENT = ["Cartao", "Dinheiro", "Boleto", "Cheque"]
+  POSSIBLE_DOC_TYPE = ["Exemplo 1", "Exemplo 2"]
 
   validates :value, presence: true, numericality: true #, message: "Precisa constar o valor da nota."
   validates :description, presence: true #, message: "Precisa constar a descrição da nota."
   validates :doc_number, presence: true, uniqueness: true #, message: "Precisa constar o numero do documento."
-  validates :form_payment, inclusion_of: possible_form_payment
-  #validates :doc_type, inclusion_of: possible_doc_type
+  validates :form_payment, :inclusion => { :in  => POSSIBLE_FORM_PAYMENT }
+  validates :doc_type, :inclusion => { :in => POSSIBLE_DOC_TYPE}
 
   belongs_to :cashier
 
--- spec/features/release_crud_spec.rb
+++ spec/features/release_crud_spec.rb
@@ -4,10 +4,10 @@ require 'spec_helper'
 new_release = '/cashiers/:cashier_id/releases/new'
 index = '/cashiers'
 
-features 'CRUD release' do
+feature 'CRUD release' do
 
 	scenario 'create Release when everything is already registered and with valid data' do
-		create(:release)
+		create(:cashier)	
 
 		visit new_release
 
@@ -15,7 +15,7 @@ features 'CRUD release' do
 		fill_in 'release[doc_number]', with: 666
 		fill_in 'release[date_release]', with: Date.now
 		fill_in 'release[description]', with: 'McDonalds'
-		fill_in 'release[form_payment]', with: 'Card'
+		fill_in 'release[form_payment]', with: 'Cartao'
 		fill_in 'release[doc_type]', with: 'Example 1'
 
 	end	
