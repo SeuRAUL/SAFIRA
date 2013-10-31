c530295a018ace0b0685718827509b839254874c
SeuRAUL <kenny.is.inmortal@gmail.com>
2013-10-31 13:43:59 -0300

    update de factoryes de Release e cashier

--- spec/factories/cashiers.rb
+++ spec/factories/cashiers.rb
@@ -5,9 +5,9 @@ FactoryGirl.define do
     # opening_balance 0.0
     # releases
     # enterprise
-    factory :cashier do
-    	opening_balance 0.0
-    	releases nil
-    	enterprise FactoryGirl.build(:enterprise)
-  	end
+  factory :cashier do
+	opening_balance 0.0
+	releases nil
+	enterprise FactoryGirl.build(:enterprise)
+  end
 end
