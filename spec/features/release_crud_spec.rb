fdfe14f8e7a2dba9aff2054e133dde413e04600d
SeuRAUL <kenny.is.inmortal@gmail.com>
2013-11-27 16:42:40 -0300

    teste feature release

--- spec/features/release_crud_spec.rb
+++ spec/features/release_crud_spec.rb
@@ -1,13 +1,15 @@
 # -*- encoding : utf-8 -*-
 require 'spec_helper'
 
-new_release = '/cashiers/:cashier_id/releases/new'
+new_release = '/cashiers/1/releases/new'
 index = '/cashiers'
 
 feature 'CRUD release' do
 
 	scenario 'create Release when everything is already registered and with valid data' do
-		create(:cashier)	
+		@current_user = create(:enterprise)
+		
+		#create(:cashier)	
 
 		visit new_release
 
@@ -15,12 +17,12 @@ feature 'CRUD release' do
 		fill_in 'release[doc_number]', with: 666
 		fill_in 'release[date_release]', with: '2013-10-31'
 		fill_in 'release[description]', with: 'McDonalds'
-		fill_in 'release[form_payment]', with: 'Cartao'
-		fill_in 'release[doc_type]', with: 'Recibo'
-		fill_in 'release[type_release]', with: 'Saida'
+		#fill_in 'release[form_payment]', with: 'Cartao'
+		#fill_in 'release[doc_type]', with: 'Recibo'
+		#fill_in 'release[type_release]', with: 'Saida'
 		fill_in 'release[origin_destination]', with: 'Origem'
 
-		click_button('Cadastrar')
+		click_button("Cadastrar")
 
 		page.should have_content('Release was successfully created.')
 
