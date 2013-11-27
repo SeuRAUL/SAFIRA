e8f55b708dc89f73426fa1c9cd7b7c15285177cf
andrezap <andrezapbl1@gmail.com>
2013-11-27 15:24:37 -0300

    mudanças no teste de feature de release

--- spec/features/release_crud_spec.rb
+++ spec/features/release_crud_spec.rb
@@ -1,23 +1,33 @@
 # -*- encoding : utf-8 -*-
 require 'spec_helper'
 
-new_release = '/cashiers/:cashier_id/releases/new'
+
+new_release = '/cashiers/1/releases/new'
 index = '/cashiers'
 
 feature 'CRUD release' do
+	include Features::SessionHelpers
 
-	scenario 'create Release when everything is already registered and with valid data' do
-		create(:cashier)	
+	before do 
+		@enterprise = create(:enterprise)
+		sign_in(@enterprise)
+	end
+	
+	scenario 'create Release when everything is already registered and with valid data' do						
+		create(:cashier, enterprise: @enterprise)	
 
-		visit new_release
+		visit(new_release)
 
 		fill_in 'release[value]', with: 19.50
 		fill_in 'release[doc_number]', with: 666
 		fill_in 'release[date_release]', with: '2013-10-31'
 		fill_in 'release[description]', with: 'McDonalds'
-		fill_in 'release[form_payment]', with: 'Cartao'
-		fill_in 'release[doc_type]', with: 'Recibo'
-		fill_in 'release[type_release]', with: 'Saida'
+		#fill_in 'release[form_payment]', with: 'Cartao'
+		#fill_in 'release[doc_type]', with: 'Recibo'
+		#fill_in 'release[type_release]', with: 'Saida'
+		select('Dinheiro', from: 'release[form_payment]')
+		select('Recibo', from: 'release[doc_type]')
+		select('Saida', from: 'release[type_release]')
 		fill_in 'release[origin_destination]', with: 'Origem'
 
 		click_button('Cadastrar')
