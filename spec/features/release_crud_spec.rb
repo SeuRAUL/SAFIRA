fa422ba9e44bad584bb65f7298e1f8d9fa8b4ef1
Hivana <hivanamaced@gmail.com>
2013-11-28 03:30:37 -0300

    tests

--- spec/features/release_crud_spec.rb
+++ spec/features/release_crud_spec.rb
@@ -8,19 +8,62 @@ feature 'CRUD release' do
 	include Features::SessionHelpers
  	
  	background do
- 		
     	@user = FactoryGirl.create :enterprise
     	sign_in @user
     	@cashier = FactoryGirl.create :cashier, enterprise: @user
+    	@release = FactoryGirl.create :release, cashier: @cashier
+    	visit new_release
+      	fill_in 'release[value]', with: 19.50
+      	fill_in 'release[doc_number]', with: 666
+      	fill_in 'release[date_release]', with: '2013-10-31'
+      	fill_in 'release[description]', with: 'McDonalds'
+      	select 'Dinheiro', from: 'release[form_payment]'
+      	select 'Recibo', from: 'release[doc_type]'
+      	select 'Saida', from: 'release[type_release]'
+      	fill_in 'release[origin_destination]', with: 'Origem'
   	end
 
   context 'Index' do
-    background do
-      @release = FactoryGirl.create :release, cashier: @cashier
-      visit '/cashiers/1/releases/new'
-    end
     scenario 'creating a new release' do
       expect(page).to have_content 'Nova nota fiscal'
+    end
+  end
+
+  context 'Create' do
+    background do
+    	click_on 'Cadastrar'
+    end
+	scenario 'with correct inputs - create' do
+	   	expect(page).to have_content 19.50
+	   	expect(page).to have_content 666
+	   	expect(page).to have_content '2013-10-31'
+	   	expect(page).to have_content 'McDonalds'
+	   	expect(page).to have_content 'Dinheiro'
+	   	expect(page).to have_content 'Recibo'
+	   	expect(page).to have_content 'Saida'
+	   	expect(page).to have_content 'Origem'
+	end
+  end
+
+  context 'Update' do
+    background do
+      click_on 'Cadastrar'
+      click_on 'EDITAR'
+    end
+    scenario 'with correct inputs - update' do
+    	fill_in 'release[description]', with: 'Subway'
+    	click_on 'Cadastrar'
+    	expect(page).to have_content 'Subway'
+    end
+  end
+
+  context 'Destroy' do
+    background do
+      click_on 'Cadastrar'
+      click_on 'EXCLUIR'
+    end
+    scenario 'successfully' do
+      #click_on 'EXCLUIR'
     end
   end
 
